51626c043f75b92faf44c49411980f583a78bb34
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-06-29 16:08:50 +0600

    Fetch Post data and Show them

--- next-hero/src/app/Post/page.jsx
+++ next-hero/src/app/Post/page.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-const page = () => {
-  return <div></div>;
+const getData = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+  return data;
+};
+
+const page = async () => {
+  const data = await getData();
+  console.log(data);
+  return (
+    <div>
+      <p className="text-2xl font-semibold text-center py-5">All Data Here</p>
+      <div className="my-5 grid grid-cols-3 gap-5 px-5">
+        {data.map(({ title, body, id }) => (
+          <div key={id} className="border border-orange-600 p-3 rounded">
+            <p>
+              <span className="font-medium">Title :</span> {title}
+            </p>
+            <p>
+              <span className="font-medium pt-3">Description :</span> {body}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default page;
